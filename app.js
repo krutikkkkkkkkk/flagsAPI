c4347919c64b54b20a9f9a13066a8d34f54d0051
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-07-05 13:32:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+require('dotenv').config()
+const flags = require('./flags');
+
+
+app.get('/', (req, res)=> {
+    var path = require('path');
+    res.sendFile(path.resolve('./index.html'));
+})
+
+app.get('/api/flags', (req, res)=> {
+    res.send(flags)
+})
+
+app.get('/api/flag/:flag', (req, res)=> {
+    const flag = flags.find(f => f.flag === req.params.flag)
+    if (!flag) res.status(404).send('Invalid Flag')
+    
+    res.send(flag)
+})
+
+app.get('/api/flagName/:name', (req, res)=> {
+    const name = flags.find(n => n.name === capitalizeFirstLetter(req.params.name))
+    if (!name) res.status(404).send('Invalid Name')
+    
+    res.send(name)
+})
+
+app.get('/api/flagCode/:code', (req, res)=> {
+    const code = flags.find(c => c.code === capitalizeLetter(req.params.code))
+    if (!code) res.status(404).send('Invalid Code')
+    
+    res.send(code)
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, ()=> {
+    console.log(`Listening Port on ${port}`)
+})
+
+
+///Flag Name
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+///Flag Code
+function capitalizeLetter(string) {
+    return string.toUpperCase();
+  }
